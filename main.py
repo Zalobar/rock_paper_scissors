45f318939f5f8cec0a57f610535160efb393f202
zalobar <manley@walklore.com>
2025-06-21 11:49:31 -0400

    c: "Update"

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ while True:
     # player input loop
     while True:
         print("Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit")
-        player_move = print(input(">"))
+        player_move = print(input("> "))
         if player_move == "q":
             sys.exit()
         if player_move == "r" or player_move == "p" or player_move == "s":
@@ -37,4 +37,30 @@ while True:
     elif move_number == 2:
         computer_move = "p"
         print("PAPER")
+    elif move_number == 3:
+        computer_move = "s"
+        print("SCISSORS")
+
+    # Calculate and display Wins, Losses, and Ties
+    if player_move == computer_move:
+        print("It's a tie!")
+        ties += 1
+    elif player_move == "r" and computer_move == "s":
+        print("You win!")
+        wins += 1
+    elif player_move == "p" and computer_move == "r":
+        print("You win!")
+        wins += 1
+    elif player_move == "s" and computer_move == "p":
+        print("You win!")
+        wins += 1
+    elif player_move == "r" and computer_move == "p":
+        print("Sorry! You lose.")
+        losses += 1
+    elif player_move == "p" and computer_move == "s":
+        print("Sorry! You lose.")
+        losses += 1
+    elif player_move == "s" and computer_move == "r":
+        print("Sorry! You lose.")
+        losses += 1
     
